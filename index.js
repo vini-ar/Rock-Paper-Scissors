ffc0d9741c3fcb2ecfcdf90206813c6572440a82
Marcus Vinicyus <125088362+vini-ar@users.noreply.github.com>
2024-08-13 16:04:00 -0300

    everything ok

--- index.js
+++ index.js
@@ -7,82 +7,90 @@
 /* Display Match Winner */
 
 
+const rockButton = document.getElementById("rock");
+const paperButton = document.getElementById("paper")
+const scissorsButton = document.getElementById("scissors");
+const resultDisplay = document.getElementById("result")
+const buttons = document.querySelectorAll("button")
+
 const ACTIONS = ["rock", "paper", "scissors"]
-let humanSelection;
-let machineSelection;
+
+let humanSelection = null;
+let machineSelection = null;
+
 let round = 1;
 let humanScore = 0;
 let machineScore = 0;
 
-function askPlayerAction() {
-    while (!ACTIONS.includes(humanSelection)) {
-        humanSelection = prompt("Choose your Action: ");
-        humanSelection = humanSelection.toLowerCase();
-    }
-}
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        if (ACTIONS.includes(button.id)) {
+            humanSelection = button.id
+            playRound();
+            console.log(machineSelection)
+        }
+        else {
+            console.error()
+        }
+        round += 1
+    })
+})
 
-function chooseRandomActionForMachine() {
+function getMachineSelection() {
     let index = Math.floor(Math.random() * 3);
     machineSelection = ACTIONS[index]
 
 }
 
-function displayRoundResult() {
+function computeRoundResult() {
 
     /* Rock Winner over Scissors*/
     if (humanSelection === ACTIONS[2] && machineSelection === ACTIONS[0]) {
         machineScore += 1;
-        console.log(`Machine Win Round ${round}`)
+        resultDisplay.textContent += `Machine Win Round ${round}`
     }
     /* Paper Winner over Rock*/
     else if (humanSelection === ACTIONS[0] && machineSelection === ACTIONS[1]) {
         machineScore += 1;
-        console.log(`Machine Win Round ${round}`)
+        resultDisplay.textContent += `Machine Win Round ${round}`
     }
     /* Scissors Winner over Paper*/
 
     else if (humanSelection === ACTIONS[1] && machineSelection === ACTIONS[2]) {
         machineScore += 1;
-        console.log(`Machine Win Round ${round}`)
+        resultDisplay.textContent += `Machine Win Round ${round}`
     }
     else if (humanSelection === machineSelection) {
-        console.log("Draw")
+        resultDisplay.textContent += "Draw"
     }
     else {
-        console.log(`Human Win Round ${round }`)
+        resultDisplay.textContent += `Human Win Round ${round}`
         humanScore += 1;
     }
 
 }
-function displayMatchWinner() {
 
-    if (humanScore > machineScore) {
-        console.log("Human Won!")
+function resetRound() {
+    round = 1;
+    humanScore = 0;
+    machineScore = 0;
+}
+
+function displayGameWinner() {
+    if (humanScore === 5) {
+        alert("Human Won!")
+        resetRound()
     }
-    else if (machineScore > humanScore) {
-        console.log("Machine Won!")
-    }
-    else {
-        console.log("Draw")
+    else if (machineScore === 5) {
+        alert("Machine Won!")
+        resetRound()
     }
 }
 
 
-function playGame() {
-    while (round < 6) {
-        askPlayerAction()
-        chooseRandomActionForMachine()
-        displayRoundResult()
-        round += 1;
-        humanSelection = null;
-    }
-    displayMatchWinner();
-    humanSelection = null;
-    machineSelection = null;
-    round = null;
-    humanScore = null;
-    machineScore = null;
-   
-}
 
-playGame()
+function playRound() {
+    getMachineSelection()
+    computeRoundResult()
+    displayGameWinner()
+}
